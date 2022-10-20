c1a081db4b8ff6dc6240625e5548f2dcb4a3ba63
lokeshvasnik <lokeshvasnik2003@gmail.com>
2022-10-20 15:36:32 +0530

    Added Commments

--- Js/app.js
+++ Js/app.js
@@ -1,31 +1,34 @@
+// Selecting elements
 const searchBtn = document.querySelector('.search__btn');
 
-const result = document.querySelector('.advices');
-
 searchBtn.addEventListener('click', (e) => {
-  const loaderDiv = document.getElementById('loader');
+  const result = document.querySelector('.advices');
+
+  const loaderDiv = document.querySelector('.loader');
 
   const loading = document.querySelector('.loading');
 
+  // Api Url
   let finalURL = `https://api.adviceslip.com/advice`;
 
+  // Hide and Unhide functions
   const showLoader = () => {
     loaderDiv.classList.add('show');
     loading.innerHTML = 'Loading Advice';
     result.innerHTML = '';
   };
-  const hideLoader = () => {
-    loaderDiv.classList.remove('show');
-  };
+  const hideLoader = () => loaderDiv.classList.remove('show');
 
   // Fetching data from the rest api
 
+  // Gets called when the advice is loading
   showLoader();
 
   setTimeout(() => {
     fetch(finalURL)
       .then((response) => response.json())
       .then((data) => {
+        // Gets called wehn the advice loads
         hideLoader();
         result.innerHTML = `
         <p> ${data.slip.advice} </p>
